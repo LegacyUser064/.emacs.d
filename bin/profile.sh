854e12def3e334edcfc37d91b209572dd51add8f
Russell Smith <russell.smith7502@gmail.com>
2021-06-21 20:18:21 -0500

    Hard code elisp profile script location in profile.sh

--- bin/profile.sh
+++ bin/profile.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 EMACS_BIN="emacs"
-PROFILER="../local/packages/profile-dotemacs/profile-dotemacs.el"
+PROFILER="~/.emacs.d/local/packages/profile-dotemacs/profile-dotemacs.el"
 FUNCTION="profile-dotemacs"
 
 $EMACS_BIN -Q -l $PROFILER -f $FUNCTION
